87a86039ea8a59d2f2bd75f9c2fed51e8bdb5ffa
amliyanage <ashenmliyanage@gmail.com>
2024-08-23 15:32:30 +0530

    post more option popup navigated with my post card

--- components/js/wall/MyPostWallComponent.js
+++ components/js/wall/MyPostWallComponent.js
@@ -1,47 +1,68 @@
 $(document).ready(function() {
     LoadCards();
+    $('.postMoreOption').css('display', 'none');
 });
 
 function LoadCards(){
-    $("#myPostWallComponent .postCards").empty();
+    $("#myPostWallComponent").empty();
     for (let i = 0; i < 10; i++) {
-        $("#myPostWallComponent").append(
-            MyPostCard()
-        )
+        $("#myPostWallComponent").append(MyPostCard());
     }
 }
 
-const MyPostCard = () =>{
+var flag = false;
+
+$('#myPostWallComponent').on('click', '.postMore', function() {
+    $('#myPostWallComponent .postMoreOption').hide();
+    if (flag === false) {
+        $(this).closest('.postCard').find('.postMoreOption').css('display', 'block');
+        flag = true;
+    }
+    else {
+        $(this).closest('.postCard').find('.postMoreOption').css('display', 'none');
+        flag = false;
+    }
+});
+
+const MyPostCard = () => {
     return `
-    <div id="postCard" class="p-5 mb-3">
-            <div>
+    <div class="p-5 mb-3 postCard" id="postCard">
+        <div class="list-group postMoreOption">
+            <a href="#" class="list-group-item list-group-item-action d-flex align-items-center ">
+                <img src="/assets/icons/info.png"> <div class="option">Post Info</div>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action d-flex align-items-center">
+                <img src="/assets/icons/delete.png"> <div class="option">Delete Post</div>
+            </a>
+        </div>
+        <div>
+            <div class="d-flex justify-content-between">
+                <div class="d-flex gap-3 align-items-center">
+                    <img class="profileImage" src="../../../assets/image/profilePic.png" alt="Profile Image" style="border: 2px solid #04FF00;">
+                    <div class="d-flex flex-column">
+                        <h5 class="m-0">Abrahum Joe</h5>
+                        <h6>7 hours ago</h6>
+                    </div>
+                </div>
+                <button class="bg-transparent border-0 postMore">
+                    <img src="../../../assets/icons/moreIcon.png" alt="More Icon">
+                </button>
+            </div>
+            <div class="px-3 mx-5 mt-2">
+                <p>🤖 Revolutionizing tech, one innovation at a time. #TechTrends #Innovation </p>
                 <div class="d-flex justify-content-between">
                     <div class="d-flex gap-3 align-items-center">
-                        <img class="profileImage" src="../../../assets/image/profilePic.png" alt="Profile Image" style="border: 2px solid #04FF00;">
-                        <div class="d-flex flex-column">
-                            <h5 class="m-0">Abrahum Joe</h5>
-                            <h6>7 hours ago</h6>
-                        </div>
+                        <img src="../../../assets/icons/likeIcon.png" alt="Inspire Icon">
+                        <span>56 Insights</span>
                     </div>
-                    <button class="bg-transparent border-0">
-                        <img src="../../../assets/icons/moreIcon.png" alt="More Icon">
-                    </button>
-                </div>
-                <div class="px-3 mx-5 mt-2">
-                    <p>🤖 Revolutionizing tech, one innovation at a time. #TechTrends #Innovation </p>
-                    <div class="d-flex justify-content-between">
-                        <div class="d-flex gap-3 align-items-center">
-                            <img src="../../../assets/icons/likeIcon.png" alt="Inspire Icon">
-                            <span>56 Insights</span>
-                        </div>
-                            <div class="ms-md-auto mt-3 mt-md-0">
-                                <button type="button" class="btn btn-light fw-bold" id="edit-info-btn">
-                                    <img src="../../../assets/icons/editIcon.png" alt="Inspire Icon"> Edit Post
-                                </button>
-                            </div>
-                       </div>
+                    <div class="ms-md-auto mt-3 mt-md-0">
+                        <button type="button" class="btn btn-light fw-bold" id="edit-info-btn">
+                            <img src="../../../assets/icons/editIcon.png" alt="Inspire Icon"> Edit Post
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    `
+    </div>
+    `;
 }
