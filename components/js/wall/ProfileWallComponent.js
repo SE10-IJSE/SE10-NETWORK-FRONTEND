264a4dfc99252590c5df463f5a298b55dcc32bef
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-24 18:04:14 +0530

    profileWallComponent connected with backend

--- components/js/wall/ProfileWallComponent.js
+++ components/js/wall/ProfileWallComponent.js
@@ -1,3 +1,6 @@
+import { getUserData } from "../../../model/UserProfileModel.js";
+import { updateUserData } from "../../../model/UserProfileModel.js";
+
 function togglePassword(inputId, element) {
   const input = document.getElementById(inputId);
   const icon = element.querySelector("i");
@@ -12,7 +15,37 @@ function togglePassword(inputId, element) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+// Make the function available globally
+window.togglePassword = togglePassword;
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const token = getJwtToken();
+  let userData;
+
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    try {
+      userData = await getUserData();
+      if (userData) {
+        // Populate profile info
+        document.querySelector(".profile-info h1").textContent =
+          userData.data.name;
+        document.querySelector(
+          ".profile-info h3"
+        ).textContent = `@${userData.data.userId}`;
+        document.querySelector(".profile-info h4").textContent =
+          userData.data.bio;
+        document.querySelector("input[type='email']").value =
+          userData.data.email;
+        document.querySelector("input[type='date']").value = userData.data.dob;
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  } else {
+    console.error("User email not found in local storage");
+  }
+
   const editCoverPhotoBtn = document.getElementById("edit-cover-photo-btn");
   const editCoverPhotoMobileBtn = document.getElementById("btn-Edit");
   const editProfilePhotoBtn = document.getElementById("circle-camera");
@@ -67,4 +100,55 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Remove photo clicked");
       dropdownMenu.style.display = "none";
     });
+
+  // Adding event listener for the "Edit Details" button
+  document
+    .getElementById("update-btn")
+    .addEventListener("click", async function () {
+      const newPassword = document.getElementById("newPassword").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
+
+      if (newPassword !== confirmPassword) {
+        alert("New password and confirm password do not match!");
+        return;
+      }
+
+      // Prepare the data to be sent
+      const data = {
+        userId: userData.data.userId,
+        name: document.getElementById("name").textContent,
+        email: document.querySelector("input[type='email']").value,
+        bio: document.getElementById("bio").value,
+        password: document.getElementById("currentPassword").value,
+        newPassword: newPassword,
+        dob: document.querySelector("input[type='date']").value,
+      };
+
+      try {
+        // Delete the existing cookie named 'jwt'
+        document.cookie = "jwt=; path=/";
+
+        const response = await updateUserData(data);
+        if (response.status === 200) {
+          // Set the new cookie with the updated token
+          document.cookie = `jwt=${response.data.data.token}; path=/`;
+          alert("Profile updated successfully!");
+          window.location.reload();
+        } else if (response.status === 406) {
+          alert("Current password is incorrect.");
+        }
+      } catch (error) {
+        alert("Failed to update profile.");
+      }
+    });
 });
+
+function getJwtToken() {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    if (cookie.startsWith("jwt=")) {
+      return cookie.split("=")[1];
+    }
+  }
+  return null;
+}
--- controller/LoginPageController.js
+++ controller/LoginPageController.js
@@ -1,48 +1,88 @@
 import { postLoginData } from "../model/LoginFormModel.js";
+import { getTokenValidation } from "../model/LoginFormModel.js";
 
-const passwordInput = document.getElementById("password");
-const togglePassword = document.getElementById("togglePassword");
-const loginForm = document.getElementById("login");
-const emailField = document.getElementById("email");
+document.addEventListener("DOMContentLoaded", function () {
+  const jwtToken = getJwtToken();
 
-togglePassword.addEventListener("click", function () {
-  // Toggle the type attribute
-  const type =
-    passwordInput.getAttribute("type") === "password" ? "text" : "password";
-  passwordInput.setAttribute("type", type);
+  if (jwtToken) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
 
-  // Toggle the icon
-  this.classList.toggle("ri-eye-line");
-  this.classList.toggle("ri-eye-off-line");
+    getTokenValidation()
+      .then((response) => {
+        if (response.data === true) {
+          // Token is valid, redirect to the home page
+          window.location.href = "pages/homePage.html";
+        } else {
+          // Token is invalid, proceed with the login page
+          loadLoginPage();
+        }
+      })
+      .catch((error) => {
+        console.error("Token validation failed:", error);
+        // Load the login page in case of an error
+        loadLoginPage();
+      });
+  } else {
+    // No token found, load the login page
+    loadLoginPage();
+  }
 });
 
-document.getElementById("register").addEventListener("click", function () {
-  window.location.href = "pages/registrationForm.html";
-});
+function getJwtToken() {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    if (cookie.startsWith("jwt=")) {
+      return cookie.split("=")[1];
+    }
+  }
+  return null;
+}
 
-loginForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+function loadLoginPage() {
+  const passwordInput = document.getElementById("password");
+  const togglePassword = document.getElementById("togglePassword");
+  const loginForm = document.getElementById("login");
+  const emailField = document.getElementById("email");
 
-  const email = emailField.value;
-  const password = passwordInput.value;
+  togglePassword.addEventListener("click", function () {
+    // Toggle the type attribute
+    const type =
+      passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
 
-  // Send login data to the server
-  postLoginData({ email, password })
-    .then((response) => {
-      // Assuming the server response indicates success with a status code 200
-      if (response.status === 200) {
-        document.cookie = `jwt=${response.data.data.token};`;
-        axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${response.data}`;
+    // Toggle the icon
+    this.classList.toggle("ri-eye-line");
+    this.classList.toggle("ri-eye-off-line");
+  });
 
-        window.location.href = "pages/homePage.html";
-      } else {
-        alert("Login failed. Please check your email and password.");
-      }
-    })
-    .catch((error) => {
-      console.error("Login error:", error);
-      alert("An error occurred during login. Please try again later.");
-    });
-});
+  document.getElementById("register").addEventListener("click", function () {
+    window.location.href = "pages/registrationForm.html";
+  });
+
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = emailField.value;
+    const password = passwordInput.value;
+
+    // Send login data to the server
+    postLoginData({ email, password })
+      .then((response) => {
+        if (response.status === 200) {
+          document.cookie = `jwt=${response.data.data.token}; path=/`;
+          axios.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${response.data}`;
+
+          window.location.href = "pages/homePage.html";
+        } else {
+          alert("Login failed. Please check your email and password.");
+        }
+      })
+      .catch((error) => {
+        window.location.href = "/";
+        console.error("Login error:", error);
+        alert("An error occurred during login. Please try again later.");
+      });
+  });
+}
